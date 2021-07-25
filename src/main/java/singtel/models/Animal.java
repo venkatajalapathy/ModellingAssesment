25a7a25465e35446751f83907d863b4b21a97445
Venkat Krishnan <venkat1589@gmail.com>
2021-07-25 16:53:29 +0800

    Initial commit

---
+++ src/main/java/singtel/models/Animal.java
@@ -0,0 +1,8 @@
+package singtel.models;
+
+public class Animal {
+
+    public void walk() {
+        System.out.println("I am walking");
+    }
+}
---
+++ src/main/java/singtel/service/Solution.java
@@ -0,0 +1,15 @@
+package singtel.service;
+
+import singtel.models.Bird;
+
+public class Solution {
+
+    public static void main(String args[]) {
+        Bird bird = new Bird();
+
+        bird.walk();
+        bird.fly();
+        bird.sing();
+
+    }
+}
